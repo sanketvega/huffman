4a3256f15a7276ca72a1565f93383e8cec243901
Francois Aubertin <francois.aubertin@gmail.com>
2015-05-16 11:37:22 +0100

    Adding multi-characters symbols.

--- src/main/scala/Huffman.scala
+++ src/main/scala/Huffman.scala
@@ -1,7 +1,9 @@
-import java.nio.charset.{StandardCharsets, Charset}
+import java.io.File
+import java.nio.charset.StandardCharsets
 
 import scala.collection.mutable
 import scala.collection.mutable.ListBuffer
+import scala.io.Source
 
 sealed abstract class Tree {
   def nbOccurrences: Int
@@ -11,9 +13,12 @@ case class Leaf(symbol: String, nbOccurrences: Int) extends Tree
 
 object Huffman {
 
-  def nbOccurrencesBySymbol(text: String): Map[String, Int] = text
-    .groupBy(identity)
-    .map((tuple: (Char, String)) => (tuple._1.toString, tuple._2.length))
+  def nbOccurrencesBySymbol(text: String, nbChars: Int): Map[String, Int] = {
+    (0 to text.length - nbChars)
+      .map(i => text.substring(i, i+nbChars))
+      .groupBy(identity)
+      .mapValues(_.length)
+  }
 
   def buildNode(queue: mutable.PriorityQueue[Tree]) {
     val right = queue.dequeue()
@@ -49,7 +54,7 @@ object Huffman {
         case Leaf(symbol, _) => {
           decoded += symbol
           if (encoded != Nil) {
-            recurse(encoded, decoded, tree)
+            recurse(encoded.tail, decoded, tree)
           }
         }
         case Node(left, right, _) => {
@@ -64,23 +69,31 @@ object Huffman {
   }
 
   def main(args: Array[String]): Unit = {
-    val text = "i like a good pint of guinness"
+    val text = Source.fromFile(new File("/home/francois/Desktop/melville-moby-106.txt")).mkString
     val textSizeInBits = text.getBytes(StandardCharsets.UTF_8).size * 8
 
-    val symbolAndNbOccurences: Iterable[(String, Int)] = nbOccurrencesBySymbol(text)
+    val symbolAndNbOccurences: Iterable[(String, Int)] = (1 to 4)
+        .par
+        .map(nbChars => {
+          if (nbChars == 1) {
+            nbOccurrencesBySymbol(text, nbChars)
+          } else {
+            nbOccurrencesBySymbol(text, nbChars).filter(_._2 > 2)
+          }
+        })
+        .reduce(_ ++ _)
+
     val tree = buildTree(symbolAndNbOccurences)
     val mappingTable: Map[String, String] = buildMappingTable(tree).toMap
 
-    println(s"Mapping table: ${mappingTable}")
-
-    val encoded = text.map(symbol => mappingTable(symbol.toString)).mkString
-    println(encoded)
+    val encoded = text.map(symbol => mappingTable(symbol.toString)).mkString("|")
+    println(s"Encoded : ${encoded.substring(0, 500)}")
     println(s"Decoded value length in bits: $textSizeInBits")
     println(s"Encoded value length in bits: ${encoded.length}")
     println(s"Compression ratio: ${encoded.length / textSizeInBits.toDouble}")
 
     val decoded = decode(encoded, tree)
-    println(s"$decoded")
+    println(s"${decoded.substring(0, 500)}")
   }
 
 }
